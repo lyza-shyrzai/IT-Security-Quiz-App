6e395d8baa49dad465d93e2444f7902ca2243a4b
LizaSh <lizash605@gmail.com>
2017-02-19 22:00:24 +0200

    Added number of correct answers

--- app/src/main/java/com/example/androidl/itsecurityquizapp/MainActivity.java
+++ app/src/main/java/com/example/androidl/itsecurityquizapp/MainActivity.java
@@ -3,7 +3,15 @@ package com.example.androidl.itsecurityquizapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * This app is IT Security Quiz
+ */
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * The number of correct answers
+     */
+    int score = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
