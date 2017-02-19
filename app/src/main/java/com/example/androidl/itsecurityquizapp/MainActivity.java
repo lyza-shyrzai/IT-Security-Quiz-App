e31eb01ff6b102740d55c66b41ce950ce69be54d
LizaSh <lizash605@gmail.com>
2017-02-19 22:59:38 +0200

    Added "displayResult" and "createTestResult" methods

--- app/src/main/java/com/example/androidl/itsecurityquizapp/MainActivity.java
+++ app/src/main/java/com/example/androidl/itsecurityquizapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.androidl.itsecurityquizapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * This app is IT Security Quiz
@@ -24,6 +26,33 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the end test button is clicked.
      */
     public void end_Test(View view) {
+        EditText nameField = (EditText) findViewById(R.id.name_field);
+        String name = nameField.getText().toString();
 
+        // Display the test result on the screen
+        String test_result = createTestResult(name);
+        displayResult(test_result);
+
+    }
+
+    /**
+     * Create summary of the test result.
+     *
+     * @param name of the passing the test
+     * @return text of the test result
+     */
+    private String createTestResult(String name) {
+        String test_result = getString(R.string.test_result_name) + name;
+
+
+        return test_result;
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayResult(String result) {
+        TextView testResultTextView = (TextView) findViewById(R.id.test_result_text_view);
+        testResultTextView.setText(result);
     }
 }
